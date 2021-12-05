022e32b1f8f79c94a9b9e06a72b9a4b1d190e1a0
blanchar <anchar2107@gmail.com>
2021-12-05 18:30:07 +0100

    resolution seul

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,20 @@
+document.documentElement.className = "js-enabled";
 
-zaeaer
+
+//on doit decomposer le mot en plusierus lettres
+//rajouter un span a chaque lettre
+//
+
+
+const eH1 = document.querySelector('.jump');
+const eLetters = eH1.textContent;
+const aLetters = eLetters.('');
+
+const eSpan = document.createElement("span");
+for (const letter of aWords){
+
+}
+
+addEventListener('mouseover', (event)=> {
+    eSpan.style.fontSize = '80p'
+})
---
+++ src/js/note.js
@@ -0,0 +1,10 @@
+document.documentElement.className = "js-enabled";
+console.log(document.documentElement)
+// on donne la classe js-enabled au docuementElement (la racine du docuemnt) ici c'est la racine c'est le document html; voir consolelog
+
+// textContent donne la valeur du texte
+const h1 = document.querySelector('.jump');
+console.log(h1.textContent);
+
+//la methode split permet de diviser la chaine de caractere, on laisse les parenthèse vide pour que notre chaine soie convertie en un tableau de chacun de nos caractères
+
